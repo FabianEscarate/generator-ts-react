08d11b6da38378e03d3a143c6bef976de430b282
Fabian Roberto Escarate Espinoza <fescarate@outlook.com>
2018-11-02 19:29:57 -0300

    avance

---
+++ generators/app/templates/src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+let data = () => {
+    return Informacion.data;
+};
+
+ReactDOM.render(
+    <React.Fragment key='main'>
+        div
+    </React.Fragment>,
+    document.getElementById("bodyContent")
+);
